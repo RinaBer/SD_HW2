1825e99f8cae319225b18bd5a9e5000fba2547a5
ayelet <akravi@intel.com>
2016-05-12 15:34:26 +0300

    Parser implementation
    FileOrAssignment - enum with values "Unknown", "File", "Assignment"
    this enum represents the type of an entity in makefile.
    
    MakefileVertex - class with 2 fields:
    1. name (String) - the name of the file/task
    2. type (FileOrAssignment) - enum value which tells if the entity is file or task

---
+++ Makefile-308044395-308025394/makefile-app/src/main/java/makefileVertex/FileOrAssignment.java
@@ -0,0 +1,7 @@
+package makefileVertex;
+
+/**
+ * Created by akravi on 5/11/2016.
+ */
+
+public enum FileOrAssignment {Unknown, File, Assignment}
--- Makefile-308044395-308025394/makefile-app/src/main/java/parser/Parser.java
+++ Makefile-308044395-308025394/makefile-app/src/main/java/parser/Parser.java
@@ -2,17 +2,67 @@ package parser;
 
 import graph.Edge;
 import graph.Graph;
+import makefileVertex.FileOrAssignment;
+import makefileVertex.MakefileVertex;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.HashSet;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by rina.berlin on 5/7/2016.
  */
 public class Parser {
-    public static Graph<String> parse(File file) {
-        HashSet<String> vertexes = new HashSet<>();
-        HashSet<Edge> edges = new HashSet<>();
-        return new Graph<>(vertexes, edges);
+    public static Graph<MakefileVertex> parse(File file){
+        HashMap<String, FileOrAssignment> fileToType = new HashMap<>();
+        HashMap<String, List<String>> fileToDependencies = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            String[] lineArr = null;
+            FileOrAssignment type = FileOrAssignment.Unknown;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("=")){
+                    lineArr = Arrays.stream(line.split("=")).map(String::trim).toArray(String[]::new);
+                    type = FileOrAssignment.Assignment;
+                }
+                if (line.contains(":")){
+                    lineArr = Arrays.stream(line.split(":")).map(String::trim).toArray(String[]::new);
+                    type = FileOrAssignment.File;
+                }
+                String name =lineArr[0];
+                String[] filesDependencies = lineArr[1].split(",");
+                fileToType.put(name, type);
+                fileToDependencies.put(name, Arrays.asList(filesDependencies));
+                for (String fileDependency : filesDependencies) {
+                    if (!fileToType.containsKey(fileDependency))
+                        fileToType.put(fileDependency, FileOrAssignment.File);
+                }
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        HashSet<MakefileVertex> vertexes = fileToType.keySet().stream()
+                .map(key -> new MakefileVertex(key, fileToType.get(key)))
+                .collect(Collectors.toCollection(HashSet::new));
+        HashSet<Edge<MakefileVertex>> edges = new HashSet<>();
+        for (String key :
+                fileToDependencies.keySet()) {
+            for (String s :
+                    fileToDependencies.get(key)) {
+                Optional<MakefileVertex> source = vertexes.stream().filter(n -> n.GetName().equals(key)).findFirst();
+                Optional<MakefileVertex> target = vertexes.stream().filter(n -> n.GetName().equals(s)).findFirst();
+                try {
+                    edges.add(new Edge<>(source.get(), target.get()));
+                    source.get();
+                    target.get();
+                } catch (NullPointerException e){
+                    System.out.println(String.format("failed to create edge %s->%s", key, s));
+                }
+            }
+        }
+        return new Graph(vertexes, edges);
     }
 }
