a8b94b8fd5c6de1baba05653ff885535e9163c17
ayelet <akravi@intel.com>
2016-05-13 08:38:53 +0300

    fixing makefile module from fake to our MakefileImpl.
    so we can run tests

--- Makefile-308044395-308025394/makefile-app/src/main/java/cs/technion/ac/il/sd/app/MakefileModule.java
+++ Makefile-308044395-308025394/makefile-app/src/main/java/cs/technion/ac/il/sd/app/MakefileModule.java
@@ -1,10 +1,11 @@
 package cs.technion.ac.il.sd.app;
 
 import com.google.inject.AbstractModule;
+import makefileImpl.MakefileImpl;
 
 public class MakefileModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(Makefile.class).to(FakeMakefile.class);
+    bind(Makefile.class).to(MakefileImpl.class);
   }
 }
